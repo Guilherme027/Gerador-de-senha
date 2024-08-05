b8260d58489a2ad05aa66e46c2b47d41e6cf3018
David Welber <welbersolucoesti@gmail.com>
2024-08-05 12:42:15 -0300

    refactor: Improve password generation algorithm

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ let buttonElement = document.querySelector("#button");
 let sizePassword = document.querySelector("#valor");
 let password = document.querySelector("#password");
 let containerPassword = document.querySelector("#container-password");
-let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789!@";
 let novaSenha = "";
 
 
@@ -17,9 +16,15 @@ slider.oninput = function(){
 function generatePassword(){
     
     let pass = "";
-    for(let i = 0, n = charset.length; i < sliderElement.value; ++i){
-        pass += charset.charAt(Math.floor(Math.random() * n));
+
+    for (let i = 0; i < sliderElement.value; i++) {
+        
+        // Charset padrão ASCII 33-126, leia mais em:
+        // https://www.w3schools.com/charsets/ref_html_ascii.asp
+
+        pass += String.fromCharCode(Math.floor(Math.random() * (126 - 33 + 1)) + 33); 
     }
+
     containerPassword.classList.remove("hide");
     password.innerHTML = pass;
     novaSenha = pass;
